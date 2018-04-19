1c020375a4c5a475cb0e624a2ae887a394197a91
YH31IF <mateusz.wojtyczka@ingservicespolska.pl>
2018-04-19 21:53:13 +0200

    Added first excercise for CRC

---
+++ Kata1/Kata1/PasswordVerifier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using TestApp.Exercise_1.Exceptions;
+
+namespace TestApp.Exercise_1
+{
+    public sealed class PasswordVerifier
+    {
+        public void Verify(string password)
+        {
+            if (password == null)
+            {
+                throw new NullPasswordException();
+            }
+
+            if (password.Length < 9)
+            {
+                throw new ToShortPasswordException();
+            }
+
+            if (!password.Any(Char.IsUpper))
+            {
+                throw new NoUpperCaseException();
+            }
+
+            if (!password.Any(Char.IsLower))
+            {
+                throw new NoLowerCaseException();
+            }
+
+            if (!password.Any(Char.IsDigit))
+            {
+                throw new NoDigitException();
+            }
+        }
+    }
+}
---
+++ Kata1/Kata1/PasswordVerifierTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using TestApp.Exercise_1.Exceptions;
+
+namespace TestApp.Exercise_1
+{
+    [TestFixture]
+    public sealed class VerifyTests
+    {
+        private PasswordVerifier _passwordVerifier;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _passwordVerifier = new PasswordVerifier();
+        }
+
+
+        [Test]
+        public void ShouldThrowExceptionIfPasswordHasNotAtLeastEightCharacters()
+        {
+            const string password = "aaaaaa";
+
+            // act
+            void Execute()
+            {
+                _passwordVerifier.Verify(password);
+            }
+
+            // assert
+            Assert.That(Execute, Throws.InstanceOf<ToShortPasswordException>());
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfPasswordIsNull()
+        {
+            // arrange
+            const string password = null;
+
+            // act
+            void Execute()
+            {
+                _passwordVerifier.Verify(password);
+            }
+
+            // assert
+            Assert.That(Execute, Throws.InstanceOf<NullPasswordException>());
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfPasswordHasNotUpperCase()
+        {
+            // arrange
+            const string password = "tetssffffffffffff";
+
+            // act
+            void Execute()
+            {
+                _passwordVerifier.Verify(password);
+            }
+
+            // assert
+            Assert.That(Execute, Throws.InstanceOf<NoUpperCaseException>());
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfPasswordHasNotLowerCase()
+        {
+            // arrange
+            const string password = "HHHHHHHHHHHHHHHHHHHHHH";
+
+            // act
+            void Execute()
+            {
+                _passwordVerifier.Verify(password);
+            }
+
+            // assert
+            Assert.That(Execute, Throws.InstanceOf<NoLowerCaseException>());
+        }
+    }
+}
