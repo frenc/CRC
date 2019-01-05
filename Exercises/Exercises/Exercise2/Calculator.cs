ba0bfa85f01759590cfd736afd1c1cac66166b11
YH31IF <mateusz.wojtyczka@ing.com>
2019-01-05 16:34:59 +0100

    Ex 3

--- Exercises/Exercises/Exercise2/Calculator.cs
+++ Exercises/Exercises/Exercise2/Calculator.cs
@@ -1,10 +1,22 @@
-﻿namespace Exercises.Exercise2
+﻿using System;
+
+namespace Exercises.Exercise2
 {
     public sealed class Calculator
     {
         public decimal Sum(decimal firstNumber, decimal secondNumber)
         {
             return firstNumber + secondNumber;
+        }
+
+        public decimal Divide(decimal firstNumber, decimal secondNumber)
+        {
+            if (secondNumber == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return firstNumber / secondNumber;
         }
     }
 }
--- Exercises/Exercises/Exercise2/CalculatorDivisionTests.cs
+++ Exercises/Exercises/Exercise2/CalculatorDivisionTests.cs
@@ -1,9 +1,49 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Exercises.Exercise2
 {
     [TestFixture]
     public sealed class CalculatorDivisionTests
     {
+        private Calculator _calculator;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TestCase(1, 2, 0.5)]
+        [TestCase(10, 10, 1)]
+        [TestCase(100, 10, 10)]
+        public void ItShouldDivideTwoNumbers(decimal firstNumber, decimal secondNumber, decimal expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = _calculator.Divide(firstNumber, secondNumber);
+
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void ItShouldThrowExceptionWhenDivideByZero()
+        {
+            // Arrange
+            const decimal firstNumber = 12.3m;
+            const decimal secondNumber = 0m;
+
+            // Act
+            void Execute()
+            {
+                _calculator.Divide(firstNumber, secondNumber);
+            }
+
+            // Assert
+            Assert.That(Execute, Throws.InstanceOf<DivideByZeroException>());
+        }
     }
 }
